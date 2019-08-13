5cff788e7cca5a62469be45fd7d73e911c5f660f
Sophie Kim <270010565@bun2-206-17.local>
2019-08-14 11:50:16 +1200

    today lesson

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,53 @@
 $(document).ready(function() {
-    console.log( "ready!" );
+
+  //
+  //
+  // $.ajax({
+  //   type: 'GET',
+  //   url: `http://192.168.33.10:3000/people`,
+  //   dataType: 'json',
+  //   success: function(maleData){
+  //     console.log(maleData);
+  //   },
+  //   error: function (error) {
+  //     console.log(error);
+  //     console.log('something went wrong');
+  //   }
+  // });
+  //
+  //
+  // $("#male").click(function(){
+  //     $(".result").append(maleUrl)
+  // });
+  // $(".genderRadio").click(function(){
+  //   const gender = $(this).val();
+  //   console.log(gender);
+  //   if (gender === 'Male') {
+  //
+  //   } else if (gender === 'Female') {
+  //
+  //   }
+  // });
+  //
+
+
+//shorter
+
+$(".genderRadio").click(function(){
+  const gender =$(this).val().toLowerCase();
+  $.ajax({
+    url:`http://192.168.33.10:3000/gender/g=${gender}`,
+    dataType: 'json',
+    type: 'GET',
+    success: function(data){
+      console.log(data);
+    },
+    error: function(error){
+      console.log(error);
+    }
+  })
+})
+
+
+
 });
--- server.js
+++ server.js
@@ -3,21 +3,44 @@ const app = express();
 const port = 3000;
 const path = require('path')
 
+const data = require('./MOCK_DATA')
+
 app.use(function(req,res,next){
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
 
-
 app.use(express.static('./public'));
-// app.get('/about',function(req,res){
-//
-// });
+
+app.get('/about', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/about.html'));
+});
 
 
 app.use('/bootstrap',express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
 app.use('/jquery',express.static(path.join(__dirname, 'node_modules/jquery/dist')));
-app.use('/popper.js',express.static(path.join(__dirname,'node_modules/popper.js/dist')));
+app.use('/popper',express.static(path.join(__dirname,'node_modules/popper.js/dist/umd')));
+
+
+app.get('/people',function(req,res){
+  res.json(data);
+});
+
+app.get('/gender/g=:gender', function(req,res){
+  const genderParam = req.params.gender;
+  if ((genderParam === "male") || (genderParam === "female")) {
+    let filteredData =[];
+    for (var i = 0; i < data.length; i++) {
+      if (genderParam === data[i].gender.toLowerCase()) {
+        filteredData.push(data[i]);
+      }
+    }
+    res.send(filteredData);
+  } else {
+    res.send('Invalid')
+  }
+
+});
 // app.get('/',(req,res) => res.send('Hello World'));
 // app.get('/about',(req,res) => res.send('This is the about page'));
 // app.get('/contact',(req,res) => res.send('This is the contact page'));
@@ -26,11 +49,5 @@ app.use('/popper.js',express.static(path.join(__dirname,'node_modules/popper.js/
 
 
 // app.post();
-
-
-
-
-
-
 
 app.listen(port, ()=> console.log(`application is running on port ${port}`));
