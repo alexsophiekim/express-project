30f018ce9e655fc472e7c4d52b5409e248bc21ad
Sophie Kim <270010565@bun2-206-17.local>
2019-08-14 11:59:52 +1200

    longway and shortway updated

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,52 @@
 $(document).ready(function() {
 
-  //
-  //
-  // $.ajax({
-  //   type: 'GET',
-  //   url: `http://192.168.33.10:3000/people`,
-  //   dataType: 'json',
-  //   success: function(maleData){
-  //     console.log(maleData);
-  //   },
-  //   error: function (error) {
-  //     console.log(error);
-  //     console.log('something went wrong');
-  //   }
-  // });
-  //
-  //
-  // $("#male").click(function(){
-  //     $(".result").append(maleUrl)
-  // });
-  // $(".genderRadio").click(function(){
-  //   const gender = $(this).val();
-  //   console.log(gender);
-  //   if (gender === 'Male') {
-  //
-  //   } else if (gender === 'Female') {
-  //
-  //   }
-  // });
-  //
+  $.ajax({
+    type: 'GET',
+    url: `http://192.168.33.10:3000/people`,
+    dataType: 'json',
+    success: function(dataFromServer){
+      console.log(dataFromServer);
+    },
+    error: function (error) {
+      console.log(error);
+      console.log('something went wrong');
+    }
+  });
 
+
+    //The Long way
+    // $('.genderRadio').click(function(){
+    //   const gender = $(this).val();
+    //   console.log(gender);
+    //   if(gender === 'Male'){
+    //     $.ajax({
+    //       url: 'http://192.168.33.10:3000/gender/g=male',
+    //       dataType: 'json',
+    //       type: 'get',
+    //       success:function(maleData){
+    //         console.log(maleData);
+    //       },
+    //       error:function(error){
+    //         console.log(error);
+    //         console.log('something went wrong');
+    //       }
+    //     })
+    //   } else {
+    //     $.ajax({
+    //       url: 'http://192.168.33.10:3000/gender/g=female',
+    //       dataType: 'json',
+    //       type: 'get',
+    //       success:function(femaleData){
+    //         console.log(femaleData);
+    //       },
+    //       error:function(error){
+    //         console.log(error);
+    //         console.log('something went wrong');
+    //       }
+    //     })
+    //   }
+    //
+    // })
 
 //shorter
 
